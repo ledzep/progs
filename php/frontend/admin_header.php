a10f3b1ebfe01eb87e8941ca72641054448bab10
Atul Karmarkar <amkcat9@gmail.com>
2012-03-08 13:02:43 +1100

    admin-footer

---
+++ php/frontend/admin_header.php
@@ -0,0 +1,7 @@
+<h1>Gallery Admin</h1>
+<a href="index.php">Home</a>
+<a href="admin_index.php">Admin</a>
+<a href="admin_art_list.php">Art Inventory</a>
+<a href="admin_artist_list.php">Artist Details</a>
+<a href="admin_artist_insert.php">Add Artist</a>
+<hr>
