a0463e644e69ee96097d4a8b4d41d072aa8db03d
Atul Karmarkar <amkcat9@gmail.com>
2012-03-08 22:28:40 +1100

    php file

---
+++ admin_index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Gallery Admin</title>
+</head>
+<body>
+<?php include ("admin_header.php"); ?>
+
+<h2>Welcome to Admin page</h2>
+
+<?php include ("admin_footer.php"); ?>
+</body>
+</html>
